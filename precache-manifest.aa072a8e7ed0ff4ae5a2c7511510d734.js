78a29bfdedcb7325ddc2339484d28b54bb3dfe03
Alim Keskin <AlimKeskin@Alim-keskin-228.local>
2020-06-11 21:11:09 +0300

    Updates

---
+++ precache-manifest.aa072a8e7ed0ff4ae5a2c7511510d734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ffa81b014ed88811a3470ac1e32679",
+    "url": "/demo-todolist/index.html"
+  },
+  {
+    "revision": "c919d44110ee56eaa4c3",
+    "url": "/demo-todolist/static/css/main.ba412a3c.chunk.css"
+  },
+  {
+    "revision": "2a1a46b4e8146603e88a",
+    "url": "/demo-todolist/static/js/2.e7bd9875.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-todolist/static/js/2.e7bd9875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c919d44110ee56eaa4c3",
+    "url": "/demo-todolist/static/js/main.e8191d52.chunk.js"
+  },
+  {
+    "revision": "978c273042a4cfd9adcc",
+    "url": "/demo-todolist/static/js/runtime-main.c10b5d9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
